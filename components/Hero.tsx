7fbb6e3f3145dc320f5a6420105e38ba99377eb8
naxixdigital-hue <naxixdigital@gmail.com>
2026-04-01 20:19:10 +0530

    added portfolio page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,9 @@
 import { Sparkles } from 'lucide-react';
 import Link from 'next/link';
 
-export function Hero() {
+const WHATSAPP_NUMBER = process.env.NEXT_PUBLIC_WHATSAPP_NUMBER;
 
-  const WHATSAPP_NUMBER = process.env.NEXT_PUBLIC_WHATSAPP_NUMBER;
+export function Hero() {
 
   return (
     <section className="relative h-[35rem] md:h-[50rem] flex items-end pb-20 overflow-hidden">
@@ -14,7 +14,7 @@ export function Hero() {
           backgroundImage: 'url(/shalaka.JPG)'
         }}
       >
-<div className="absolute inset-0 bg-gradient-to-r from-black/50 via-black/10 to-transparent"></div>      </div>
+    <div className="absolute inset-0 bg-gradient-to-r from-black/50 via-black/10 to-transparent"></div>      </div>
 
       {/* Content */}
       <div className="relative z-10 w-full max-w-7xl mx-auto px-4 py-4 text-left text-white">
@@ -31,7 +31,7 @@ export function Hero() {
         </p>
 
         <Link 
-          href="https://wa.me/${WHATSAPP_NUMBER}?text=Hello%21%20I%20want%20to%20book%20a%20makeup%20appointment.%0APackage%2Foccasions%3A%20%5Bplease%20fill%5D%0ADate%3A%20%5Bplease%20fill%5D%0ATime%3A%20%5Bplease%20fill%5D"
+          href={`https://wa.me/${WHATSAPP_NUMBER}?text=Hello%21%20I%20want%20to%20book%20a%20makeup%20appointment.%0APackage%2Foccasions%3A%20%5Bplease%20fill%5D%0ADate%3A%20%5Bplease%20fill%5D%0ATime%3A%20%5Bplease%20fill%5D`}
           target="_blank"
           rel="noopener noreferrer"
           className="inline-block bg-[var(--color-primary)] hover:bg-[var(--color-secondary)] text-white px-6 py-2 rounded-full transition-colors text-lg"
